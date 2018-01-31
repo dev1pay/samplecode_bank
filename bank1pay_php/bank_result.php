15f2530a2632821944ef967d0d742b2493a990c8
1Pay Developer <developer@1pay.vn>
2018-01-31 15:29:10 +0700

    Update bank_result.php

--- bank1pay_php/bank_result.php
+++ bank1pay_php/bank_result.php
@@ -1,23 +1,47 @@
 <?php
-function execPostRequest($url, $data)
-  {
-  $trans_ref = isset($_GET["trans_ref"]) ? $_GET["trans_ref"] : NULL;
-  $response_code = isset($_GET["response_code"]) ? $_GET["response_code"] : NULL;
+   $access_key = isset($_GET["access_key"]) ? $_GET["access_key"] : NULL;
+   $amount = isset($_GET["amount"]) ? $_GET["amount"] : NULL;
+   $card_name = isset($_GET["card_name"]) ? $_GET["card_name"] : NULL;
+   $card_type = isset($_GET["card_type"]) ? $_GET["card_type"] : NULL;
+   $order_id = isset($_GET["order_id"]) ? $_GET["order_id"] : NULL;
+   $order_info = isset($_GET["order_info"]) ? $_GET["order_info"] : NULL;
+   $order_type = isset($_GET["order_type"]) ? $_GET["order_type"] : NULL;
+   $request_time = isset($_GET["request_time"]) ? $_GET["request_time"] : NULL;
+   $response_code = isset($_GET["response_code"]) ? $_GET["response_code"] : NULL;
+   $response_message = isset($_GET["response_message"]) ? $_GET["response_message"] : NULL;
+   $response_time = isset($_GET["response_time"]) ? $_GET["response_time"] : NULL;
+   $trans_ref = isset($_GET["trans_ref"]) ? $_GET["trans_ref"] : NULL;
+   $trans_status = isset($_GET["trans_status"]) ? $_GET["trans_status"] : NULL;
 
-  $access_key = "";           // require your access key from 1pay
-  $secret = "";               // require your secret key from 1pay
-  $return_url = "https://localhost/bank_result.php"; // returl url
+   $secret=""; 
+   $data_result = "access_key=".$access_key
+       ."&amount=".$amount
+       ."&card_name=".$card_name
+       ."&card_type=".$card_type
+       ."&order_id=".$order_id
+       ."&order_info=".$order_info
+       ."&order_type=".$order_type
+       ."&request_time=".$request_time
+       ."&response_code=".$response_code
+       ."&response_message=".$response_message
+       ."&response_time=".$response_time
+       ."&trans_ref=".$trans_ref
+       ."&trans_status=".$trans_status;
 
-    $response_message = $decode_bankCharging["response_message"];
-   $response_code = $decode_bankCharging["response_code"];
-   $amount = $decode_bankCharging["amount"];
-
-   if($response_code == "00")
+   $signature1 = hash_hmac("sha256", $data_result, $secret);
+   $signature = isset($_GET["signature"]) ? $_GET["signature"] : NULL;
+  
+   if($signature1==$signature)
    {
-    echo $response_message."-".$amount;
-   }
-            else
-                echo $response_message;
-  }
+    if($response_code == "00")
+      {
+
+      echo $response_message."-".$amount;
+      }
+              else 
+                  echo $response_message;
+    }
+   else 
+    echo  "Sai chu ki";
   
 ?>
